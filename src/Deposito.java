2e5841ab7a7173ecb2c980db396be0705f55f3ab
Joban Mejia <js.mejiar12@uniandes.edu.co>
2025-02-16 21:06:45 -0500

    Final

--- src/Deposito.java
+++ src/Deposito.java
@@ -1,20 +1,21 @@
 public class Deposito {
     private int productosAprobados = 0;
     private final int meta;
+    private final BuzonReproceso buzonReproceso;
 
-    public Deposito(int meta) {
+    public Deposito(int meta, BuzonReproceso buzonReproceso) {
         this.meta = meta;
+        this.buzonReproceso = buzonReproceso;
     }
 
     public synchronized void agregar(Producto producto) {
         productosAprobados++;
         System.out.println("Producto almacenado en el depósito: ID=" + producto.getId());
 
-        
         if (productosAprobados >= meta) {
-            System.out.println("Se ha alcanzado la meta de productos aprobados. Generando producto FIN...");
+            System.out.println("Meta alcanzada. Enviando Producto FIN...");
             Producto fin = new Producto(EstadoProducto.FIN);
-            notifyAll();
+            buzonReproceso.agregar(fin);
         }
     }
 }
--- src/EquipoCalidad.java
+++ src/EquipoCalidad.java
@@ -8,7 +8,8 @@ public class EquipoCalidad extends Thread {
     private int fallosActuales = 0;
     private final Random random = new Random();
 
-    public EquipoCalidad(BuzonRevision buzonRevision, BuzonReproceso buzonReproceso, Deposito deposito, int totalProductos) {
+    public EquipoCalidad(BuzonRevision buzonRevision, BuzonReproceso buzonReproceso, Deposito deposito,
+            int totalProductos) {
         this.buzonRevision = buzonRevision;
         this.buzonReproceso = buzonReproceso;
         this.deposito = deposito;
@@ -21,46 +22,58 @@ public class EquipoCalidad extends Thread {
             Producto producto;
 
             // Espera semiactiva // corrección
-            while ((producto = buzonRevision.retirar()) == null) {
-                Thread.yield(); 
-                if (Main.finalizado) return; // Producto FIN, entonces salir
+            /* aqui uso waiten lugar de yield */
+            synchronized (buzonRevision) {
+                while ((producto = buzonRevision.retirar()) == null && !Main.finalizado) {
+                    try {
+                        buzonRevision.wait();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+                }
             }
+
+            /* Si la simulacion acabo termina */
+            if (Main.finalizado)
+                return;
 
             int resultadoRevision = random.nextInt(100) + 1;
             boolean esDefectuoso = resultadoRevision % 7 == 0;
 
-            if (esDefectuoso && fallosActuales < maxFallos) {
-                producto.setEstado(EstadoProducto.RECHAZADO);
-            fallosActuales++;
-            buzonReproceso.agregar(producto);
+            synchronized (this) {
 
-                System.out.println("Producto rechazado ID=" + producto.getId() + " (Fallo #" + fallosActuales + ")");} 
-            
-            else {
-                producto.setEstado(EstadoProducto.APROBADO);
-                deposito.agregar(producto);
-                System.out.println("Producto aprobado ID=" + producto.getId());
+                if (esDefectuoso && fallosActuales < maxFallos) {
+                    producto.setEstado(EstadoProducto.RECHAZADO);
+                    fallosActuales++;
+                    buzonReproceso.agregar(producto);
+                    System.out.println("Producto rechazado ID =" + producto.getId() + " (Fallo #" + fallosActuales + ")");
+                }
 
-                if (fallosActuales >= maxFallos) {
-                    System.out.println("Límite de fallos alcanzado, aprobando todos los siguientes productos.");
+                else {
+                    producto.setEstado(EstadoProducto.APROBADO);
+                    deposito.agregar(producto);
+                    System.out.println("Producto aprobado ID=" + producto.getId());
+
+                    if (fallosActuales >= maxFallos) {
+                        System.out.println("Límite de fallos alcanzado, aprobando todos los siguientes productos.");
+                    }
                 }
             }
-
-            if (fallosActuales >= maxFallos) { synchronized (buzonReproceso) {
-                System.out.println("Generando producto FIN y notificando a los hilos");
-                Main.finalizado = true;
+            /* Esto ya se hace en Deposito, por eso lo comento
+            if (fallosActuales >= maxFallos) {
+                synchronized (buzonReproceso) {
+                    System.out.println("Generando producto FIN y notificando a los hilos");
+                    Main.finalizado = true;
                     buzonReproceso.agregar(new Producto(EstadoProducto.FIN));
                     buzonReproceso.notifyAll();
-                 }
+                }
                 synchronized (buzonRevision) {
-                    buzonRevision.notifyAll(); 
+                    buzonRevision.notifyAll();
                 }
                 return;
 
-                
-                
-            }
+            } */
         }
     }
 }
-
